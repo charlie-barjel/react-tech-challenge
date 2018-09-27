cecb83183c84aaf92455fa8133dbe1349be4c7bf
Charlie Barjel <charlie.barjel@gmail.com>
2018-09-27 18:36:23 +1000

    Added header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EventRow from './EventRow';
+
+const Header = ({events}) => {
+  return (<header className="header">
+    <h1 className="title">BetEasy Front End Tech Challenge</h1>
+  </header>);
+};
+
+export default Header;
